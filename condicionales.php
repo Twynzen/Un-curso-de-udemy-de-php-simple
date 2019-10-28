fc5e272e40a44c68543523bb0a3303edebc6cbc4
Twynzen <razzziel001@gmail.com>
2019-10-27 20:29:33 -0500

    pues if

---
+++ condicionales.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 10;
+$b = 5;
+//pues un if
+if ($a > $b) {
+
+  echo $a."ES mayor que: ".$b;
+
+}else if($a == $b){
+
+  echo $a. "Es igual que: ".$b;
+
+}
+
+else{
+  echo $a. " Es menor que: ". $b;
+}
+
+
+
+ ?>
